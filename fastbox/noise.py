2a885638aef9952248a82c19b6b6aa86f4c0446f
Bruno Bizarria <91908262+Bruno-Bizarria@users.noreply.github.com>
2026-08-19 19:39:25 +0100

    Update noise function to return only noise
    Removed sigma_rms from return statement in noise generation.

--- fastbox/noise.py
+++ fastbox/noise.py
@@ -72,7 +72,7 @@ class NoiseModel(object):
         # Generate unit white noise and multiply by noise rms
         noise = np.random.normal(0., 1., self.box.Kx.shape)
         noise *= sigma_rms[np.newaxis,np.newaxis,:]
-        return noise, sigma_rms
+        return noise
         
         
         
